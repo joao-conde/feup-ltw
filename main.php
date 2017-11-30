cd6502bb1d75833e25ef447f380f04a8dc8e7568
João Conde <joaoconde_13@hotmail.com>
2017-11-30 18:35:40 +0000

    main page html and some css

---
+++ main.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en-US"> 
+    <head>
+        <title>Task Manager</title>
+        <meta charset="UTF-8">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+
+    <body>
+        <header id="header_top">
+             <h1><a href="#">Task Manager</a></h1>
+             <h2>Manage your time</h2>
+             <img src="images/feuplogo.jpeg"/>
+             <h3>Name</h3>
+             <h4><a href="#">LOGOUT</a></h4>
+           
+        </header>
+
+        <nav id="menu">
+                <ul>
+                    <li>My Tasks</li>
+                    <li>My Projects</li> 
+                    <li>My Collaborators</li> 
+                    <li>URGENT</li>
+                    <li>Settings</li>  
+                </ul>
+        </nav>
+
+        <section id="main_area">
+            <table>
+                <tr>
+                    <th>Task</th>
+                    <th>Completion</th>
+                    <th>Deadline</th>
+                    <th>Project</th>
+                </tr>
+                <tr>
+                    <td>Task1</td>
+                    <td>50%</td>
+                    <td>23/12/2017</td>
+                    <td>LTW</td>
+                </tr>
+            </table>
+        </section>
+        <footer>
+            <p id="copyright">Copyright &copy; 2017-JLJ</p>
+            <p>Task Manager</p>
+       </footer>
+
+    </body>
+
+</html>
