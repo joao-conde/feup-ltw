25bee95024504d58e2cff60fad6a63b7b2c0719a
João Conde <joaoconde_13@hotmail.com>
2017-11-30 16:05:18 +0000

    header & footer

---
+++ templates/common/footer.php
@@ -0,0 +1,5 @@
+   <footer>
+    <p id="developed">Developed by: João Conde, João Furriel and Leonardo Teixeira</p>
+   </footer>
+ </body>
+</html>
---
+++ templates/common/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Project Manager</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <header>
+      <h1>PROJECT MANAGER</h1>
+
+      <nav id="menu">
+      <ul>
+        <li>Project Manager</li>
+        <li>Task Manager</li>
+      </ul>
+    </nav>
+    
+    </header>
+   
