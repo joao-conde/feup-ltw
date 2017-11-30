b93a2bf19c758bb153acb886848008d32b1fcb27
leonardomgt <leogt-15@hotmail.com>
2017-11-30 13:55:24 +0000

    up

--- database/user.php
+++ database/user.php
@@ -1,32 +1,32 @@
 <?php
 
-function updateUserPassword($username,$encryptedPassword) {
-    global $dbh;
+    include_once("database/connection.php");
 
-    $updateQuery = 'UPDATE User SET encryptedPassword = :encryptedPassword WHERE username = :username';
-    $stmt = $dbh->prepare($updateQuery);
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':encryptedPassword', $encryptedPassword, PDO::PARAM_STR);
+    function updateUserPassword($username,$encryptedPassword) {
+        global $dbh;
+
+        $updateQuery = 'UPDATE User SET encryptedPassword = :encryptedPassword WHERE username = :username';
+        $stmt = $dbh->prepare($updateQuery);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':encryptedPassword', $encryptedPassword, PDO::PARAM_STR);
+        
+        $stmt->execute();
+        
+    }
+
+    function insertUser($username, $encryptedPassword, $fullName, $shortDescription){
+
+        global $dbh;
+        
+        $insertQuery = 'INSERT INTO User (username, encryptedPassword, fullName, shortDescription) VALUES (:username, :encryptedPassword, :fullName, :shortDescription)';
+
+        $stmt = $dbh->prepare($insertQuery);
+        $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        $stmt->bindParam(':encryptedPassword', $encryptedPassword, PDO::PARAM_STR);
+        $stmt->bindParam(':fullName', $fullName, PDO::PARAM_STR);
+        $stmt->bindParam(':shortDescription',$shortDescription, PDO::PARAM_STR);
+
+        $stmt->execute();
+    }
     
-    $stmt->execute();
-    
-}
-
-function insertUser($username, $encryptedPassword, $fullName, $shortDescription){
-
-    global $dbh;
-    
-    $insertQuery = 'INSERT INTO User (username, encryptedPassword, fullName, shortDescription) VALUES (:username, :encryptedPassword, :fullName, :shortDescription)';
-
-    $stmt = $dbh->prepare($insertQuery);
-    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
-    $stmt->bindParam(':encryptedPassword', $encryptedPassword, PDO::PARAM_STR);
-    $stmt->bindParam(':fullName', $fullName, PDO::PARAM_STR);
-    $stmt->bindParam(':shortDescription',$shortDescription, PDO::PARAM_STR);
-
-    $stmt->execute();
-}
-
-
-
 ?>
