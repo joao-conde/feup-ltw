06c6186f30a08b3f47d7ba50ffe7658a3eaddaf0
joaopedrofump <joaofurriel@MacBook-Pro-de-Joao-7.local>
2017-12-01 11:49:50 +0000

    css

--- main.php
+++ main.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
+
 <html lang="en-US"> 
     <head>
+        <script src="https://use.fontawesome.com/8518b8a976.js"></script>
         <title>Task Manager</title>
         <meta charset="UTF-8">
         <link href="css/style.css" rel="stylesheet">
@@ -8,21 +10,24 @@
 
     <body>
         <header id="header_top">
-             <h1><a href="#">Task Manager</a></h1>
-             <h2>Manage your time</h2>
-             <img src="images/feuplogo.jpeg"/>
-             <h3>Name</h3>
-             <h4><a href="#">LOGOUT</a></h4>
-           
+            <div id="logo"> 
+                <h1><img src="images/logo.svg"><a href="#">Task Manager</a></h1>
+                <h2>Manage your time</h2>
+            </div>
+            <img src="images/feuplogo.jpeg"/>
+            <div id="profile">
+                <h3><a href="#">Name Surname</a></h3>
+                <h4><a href="#">LOGOUT</a></h4>
+            </div>
         </header>
 
         <nav id="menu">
                 <ul>
-                    <li>My Tasks</li>
-                    <li>My Projects</li> 
-                    <li>My Collaborators</li> 
-                    <li>URGENT</li>
-                    <li>Settings</li>  
+                    <li><a href="#">My Tasks</a></li>
+                    <li><a href="#">My Projects</a></li> 
+                    <li><a href="#">My Collaborators</a></li> 
+                    <li><a href="#">URGENT</a></li>
+                    <li><a href="#">Settings</a></li>  
                 </ul>
         </nav>
 
@@ -35,10 +40,28 @@
                     <th>Project</th>
                 </tr>
                 <tr>
-                    <td>Task1</td>
+                    <td><a href="#">Task1</a></td>
                     <td>50%</td>
                     <td>23/12/2017</td>
-                    <td>LTW</td>
+                    <td><a href="#">LTW</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Task2</a></td>
+                    <td>10%</td>
+                    <td>1/1/2018</td>
+                    <td><a href="#">RCOM</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Task3</td>
+                    <td>70%</td>
+                    <td>5/2/2018</td>
+                    <td><a href="#">LAIG</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Task4</a></td>
+                    <td>10%</td>
+                    <td>23/3/2018</td>
+                    <td><a href="#">PLOG</a></td>
                 </tr>
             </table>
         </section>
