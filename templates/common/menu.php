f77ee895527f1d64564c8d5688e995cb43d45ed8
joaopedrofump <joaofurriel@MacBook-Pro-de-Joao-7.local>
2017-12-07 18:13:51 +0000

    menu organizado

--- templates/common/menu.php
+++ templates/common/menu.php
@@ -1,9 +1,9 @@
 <nav id="menu">
     <ul>
         <li><a href="#">My Tasks</a></li>
+        <li><a href="#">My Lists</a></li>
         <li><a href="#">My Projects</a></li> 
         <li><a href="#">My Collaborators</a></li> 
-        <li><a href="#">URGENT</a></li>
-        <li><a href="#">Settings</a></li>  
+        <li><a href="#">Urgent Tasks</a></li> 
     </ul>
 </nav>
